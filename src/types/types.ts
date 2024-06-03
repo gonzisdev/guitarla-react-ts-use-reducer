51e984638ff8be8d7fc83f9da6217092edcd4e13
Gonzalo Vasco López <gvascolopez@gmail.com>
2024-06-03 18:06:47 +0200

    first commit

---
+++ src/types/types.ts
@@ -0,0 +1,17 @@
+
+export type Guitar = {
+    id: number;
+    name: string;
+    image: string;
+    description: string;
+    price: number;
+};
+
+export type CartItem = Guitar & {
+    quantity: number;
+};
+
+// export type GuitarID = Pick<Guitar, 'id'>;
+// export type GuitarID = Omit<Guitar, 'id' | 'name'> & {
+//     quantity: number;
+// };
